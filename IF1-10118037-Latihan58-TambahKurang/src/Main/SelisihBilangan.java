612c9bc2258b6150e340ffc771a1f9c178f7801e
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-11-01 21:27:20 +0700

    Add files via upload

---
+++ IF1-10118037-Latihan58-TambahKurang/src/Main/SelisihBilangan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author Muhammad Ihsan
+ */
+public class SelisihBilangan extends Bilangan{
+    public void tampilSelisih(){
+        System.out.println("Hasil Selisih 3 - 4 = " + (getX() - getY()));
+    }
+}
---
+++ IF1-10118037-Latihan58-TambahKurang/src/Main/main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author Muhammad Ihsan
+ *  NAMA       : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM          : 10118037
+ * Deskripsi Program   : Program ini berisi program menampilkan  Tambah kurang bialangan
+ *                                       
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JumlahBilangan hasil = new JumlahBilangan();
+        hasil.tampilHasilJumlah();
+        SelisihBilangan hasil1 = new SelisihBilangan();
+        hasil1.tampilSelisih();
+    }
+    
+}
